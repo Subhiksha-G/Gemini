c0fe96db3ebb7281bdd1135f62fe3010617673dd
Subhiksha-G <subhikshavkl@gmail.com>
2026-05-07 14:10:10 +0530

    Updated js file for hero section by adding dynamic content as typing text.

--- home-script.js
+++ home-script.js
@@ -11,3 +11,39 @@ function handleContact(event) {
     feedback.style.display = "block";
     feedback.innerHTML = `Thank You, ${name}! Your message has been "sent" (simulated).`;
 }
+
+const textElement = document.getElementById("typewriter");
+
+const phrases = [
+    "Front End Developer",
+    "UI Designer",
+    "Web Enthusiast",
+    "Problem Solver"
+];
+
+let phraseIndex = 0;
+let characterIndex = 0;
+
+function type() {
+    if (characterIndex < phrases[phraseIndex].length) {
+        textElement.innerHTML += phrases[phraseIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(type, 100);  //Speed of typing
+    } else {
+        setTimeout(erase, 100);
+    }
+}
+
+function erase() {
+    if (characterIndex > 0) {
+        textElement.innerHTML = phrases[phraseIndex].substring(0, characterIndex - 1);
+        characterIndex--;
+        setTimeout(erase, 50);
+    } else {
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        setTimeout(type, 500);
+    }
+}
+
+// Start the animation when the page loads
+window.addEventListener("DOMContentLoaded", type);
